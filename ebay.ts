0e2feeff1076fa03a98e442d0faaaa373da97f67
Joshua Cline <wwjdtd@gmail.com>
2020-11-13 19:13:25 -0800

    Ebay page scraper now works.

--- ebay.ts
+++ ebay.ts
@@ -1,4 +1,4 @@
-import { DOMParser, Element, HTMLDocument } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
 export interface ebayListing {
     name: string,
@@ -10,31 +10,57 @@ export interface ebayListing {
     image: Blob,
 }
 
+function stripTNT(str: string): string {
+    return str.replaceAll("\n", "").replaceAll("\t", "")
+}
+
 export async function getOnePage(username: string, page: number) {
     const req = await fetch(`https://www.ebay.com/sch/m.html?_nkw=&_armrs=1&_from=&_ssn=${username}&_pgn=${page}&_skc=200&rt=nc`);
     const html = await req.text();
-    const doc: HTMLDocument | null = new DOMParser().parseFromString(html, "text/html");
-    if(doc === null){
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    if (!doc){
         throw new Error("Invalid HTML from ebay.com");
-    } else {
-        const uList = doc.getElementById("ListViewInner").children;
-        const items: Array<ebayListing> = [];
-        for (const uItem of uList) {
-            const name: string = uItem.getElementsByClassName("lvtitle")[0].textContent;
-            const id: string = uItem.getAttribute("listingid");
-            //const url: string = uItem.getElementsByClassName("lvtitle")[0].children[0].href;
-            // REMEMBER: THIS MAY BREAK IN THE FUTURE.
-            const price: string = uItem.getElementsByClassName("lvprice")[0].textContent;
-            // THIS IS FORMATTED WRONG (+{PRICE} shipping) NEED TO EXTRACT RAW SHIPPING.
-            const shipping: string = uItem.getElementsByClassName("lvshipping")[0].textContent;
-            const freeShip: boolean = uItem.getElementsByClassName("lvshipping")[0].textContent == "Free shipping";
-            //const imageRequest = await fetch(uItem.getElementsByTagName("img")[0].src);
-            //const image: Blob = await imageRequest.blob();
-            const listing = { name, id, /*url,*/ price, shipping, freeShip, /*image*/ };
-            return listing;
-            break;
-            //items.push(listing);
-        }
-        return items;
     }
+    const uList = doc.getElementById("ListViewInner")?.children;
+    const items: Array<ebayListing> = [];
+    if(!uList){
+        throw new Error("Can not find ul from ebay.com");
+    }
+    let missed = 0;
+    for (const uItem of uList) {
+        const name: string = stripTNT(uItem.getElementsByClassName("lvtitle")[0]?.textContent || "");
+        if(!name){
+            console.warn("Missed getting a name!");
+            missed++
+            continue;
+        }
+        const id: string = uItem.getAttribute("listingid") || "";
+        if(id == ""){
+            console.warn(`Invalid ID scraped for item ${name}`);
+            missed++
+            continue;
+        }
+        const url: string = uItem.getElementsByClassName("lvtitle")[0].children[0].getAttribute("href") || "";
+        if (url == "") {
+            console.warn(`Invalid HREF scraped for item ${name}`);
+            missed++
+            continue;
+        }
+        // REMEMBER: THIS MAY BREAK IN THE FUTURE.
+        const price: string = stripTNT(uItem.getElementsByClassName("lvprice")[0].textContent);
+        // THIS IS FORMATTED WRONG (+{PRICE} shipping) NEED TO EXTRACT RAW SHIPPING.
+        const shipping: string = stripTNT(uItem.getElementsByClassName("lvshipping")[0].textContent).replaceAll("+", "").replaceAll(" shipping", "");
+        const freeShip: boolean = shipping == "Free shipping";
+        const imageUrl = uItem.getElementsByTagName("img")[0].getAttribute("src") || "";
+        if (id == "") {
+            console.warn(`Invalid SRC scraped for item's image ${name}`);
+            missed++
+            continue;
+        }
+        const imageRequest = await fetch(imageUrl);
+        const image: Blob = await imageRequest.blob();
+        const listing = { name, id, url, price, shipping, freeShip, image };
+        items.push(listing);
+    }
+    return [items, missed];
 }
