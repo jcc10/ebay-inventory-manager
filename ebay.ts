d06ce5e88aa1be9e0429fa75951c372ba443eec3
Joshua Cline <wwjdtd@gmail.com>
2020-11-13 19:37:04 -0800

    Some final touches on the ebay scraper.

--- ebay.ts
+++ ebay.ts
@@ -10,7 +10,7 @@ export interface ebayListing {
     image: Blob,
 }
 
-function stripTNT(str: string): string {
+function stripTnT(str: string): string {
     return str.replaceAll("\n", "").replaceAll("\t", "")
 }
 
@@ -26,35 +26,35 @@ export async function getOnePage(username: string, page: number) {
     if(!uList){
         throw new Error("Can not find ul from ebay.com");
     }
-    let missed = 0;
+    let skipped = 0;
     for (const uItem of uList) {
-        const name: string = stripTNT(uItem.getElementsByClassName("lvtitle")[0]?.textContent || "");
+        const name: string = stripTnT(uItem.getElementsByClassName("lvtitle")[0]?.textContent || "");
         if(!name){
             console.warn("Missed getting a name!");
-            missed++
+            skipped++
             continue;
         }
         const id: string = uItem.getAttribute("listingid") || "";
         if(id == ""){
             console.warn(`Invalid ID scraped for item ${name}`);
-            missed++
+            skipped++
             continue;
         }
         const url: string = uItem.getElementsByClassName("lvtitle")[0].children[0].getAttribute("href") || "";
         if (url == "") {
             console.warn(`Invalid HREF scraped for item ${name}`);
-            missed++
+            skipped++
             continue;
         }
         // REMEMBER: THIS MAY BREAK IN THE FUTURE.
-        const price: string = stripTNT(uItem.getElementsByClassName("lvprice")[0].textContent);
+        const price: string = stripTnT(uItem.getElementsByClassName("lvprice")[0].textContent);
         // THIS IS FORMATTED WRONG (+{PRICE} shipping) NEED TO EXTRACT RAW SHIPPING.
-        const shipping: string = stripTNT(uItem.getElementsByClassName("lvshipping")[0].textContent).replaceAll("+", "").replaceAll(" shipping", "");
-        const freeShip: boolean = shipping == "Free shipping";
+        const shipping: string = stripTnT(uItem.getElementsByClassName("lvshipping")[0].textContent).replaceAll("+", "").replaceAll(" shipping", "");
+        const freeShip: boolean = shipping == "Free";
         const imageUrl = uItem.getElementsByTagName("img")[0].getAttribute("src") || "";
         if (id == "") {
             console.warn(`Invalid SRC scraped for item's image ${name}`);
-            missed++
+            skipped++
             continue;
         }
         const imageRequest = await fetch(imageUrl);
@@ -62,5 +62,20 @@ export async function getOnePage(username: string, page: number) {
         const listing = { name, id, url, price, shipping, freeShip, image };
         items.push(listing);
     }
-    return [items, missed];
+    let maxPageSeen: number = 1;
+    {
+        const pages = doc.getElementsByClassName("pages")[0].children;
+        if(pages){
+            const lastPage = stripTnT(pages[pages.length - 1].textContent || "");
+            if(lastPage != ""){
+                const lp = parseInt(lastPage);
+                if(!isNaN(lp)){
+                    maxPage = lp;
+                }
+            }
+        }
+        
+    }
+    
+    return { items, skipped, maxPageSeen};
 }
